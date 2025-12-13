9d95ff673d330802dc5859de91e7b1b360aa3088
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-12-13 23:41:42 +0600

    Add reject tutor button function

--- src/pages/dashboard/student/AppliedTutors.jsx
+++ src/pages/dashboard/student/AppliedTutors.jsx
@@ -19,13 +19,8 @@ const AppliedTutors = () => {
     },
   });
 
-  // Counts
-  const totalApplication = tuitions.length;
-  const totalPending = tuitions.filter((t) => t.status === 'pending').length;
-  const totalSelected = tuitions.filter((s) => s.status === 'selected').length;
-
   // application fetch
-  const { data: applications = [] } = useQuery({
+  const { data: applications = [], refetch } = useQuery({
     queryKey: ['applications', user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get('/tutor-applications');
@@ -33,14 +28,42 @@ const AppliedTutors = () => {
     },
   });
 
-  console.log(applications);
+  // Counts
+  const totalApplication = applications.length;
+  const totalPending = applications.filter((t) => t.applyStatus === 'pending').length;
+  const totalSelected = applications.filter((s) => s.applyStatus === 'selected').length;
+  const totalRejected = applications.filter((s) => s.applyStatus === 'rejected').length;
+
+  const handleRejectBtn = async (application) => {
+    const id = application._id;
+
+    try {
+      await axiosSecure.patch(`/applications/${id}`, {
+        applyStatus: 'rejected',
+      });
+
+      alert('You rejected this application successfully!');
+
+      refetch();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //  pending applications tuitionId
+  const tuitionIds = applications
+    .filter((application) => application.applyStatus === 'pending')
+    .map((application) => application.tuitionId);
+
+  // only pending application
+  const appliedTuitions = tuitions.filter((tuition) => tuitionIds.includes(tuition._id));
 
   return (
     <Container>
       <section className="my-10 bg-base-200/60 rounded-4xl px-4 sm:px-6 py-10">
         <div className="max-w-6xl mx-auto">
           {/* Header */}
-          <div className="bg-base-100 rounded-full shadow-[0_18px_45px_rgba(15,26,51,0.08)] border border-base-200 px-5 sm:px-8 py-4 sm:py-5 text-center w-full max-w-3xl mx-auto">
+          <div className="bg-base-100 rounded-2xl shadow-[0_18px_45px_rgba(15,26,51,0.08)] border border-base-200 px-5 sm:px-8 py-4 sm:py-5 text-center w-full max-w-3xl mx-auto">
             <div className="flex items-center justify-center gap-3">
               <h3 className="text-xl sm:text-2xl font-semibold text-base-content">Applied Tutors</h3>
               <span className="w-9 h-9 rounded-full bg-accent/70 flex items-center justify-center text-secondary">
@@ -51,7 +74,7 @@ const AppliedTutors = () => {
           </div>
 
           {/* Stats */}
-          <div className="mt-8 sm:mt-10 grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-5 max-w-3xl mx-auto">
+          <div className="mt-8 sm:mt-10 grid grid-cols-1 sm:grid-cols-4 gap-4 sm:gap-5 max-w-3xl mx-auto">
             {/* Total Applications */}
             <div className="bg-base-100 border border-base-200 rounded-2xl p-4 sm:p-5 text-center shadow-[0_12px_30px_rgba(15,26,51,0.06)]">
               <p className="text-xs sm:text-sm text-neutral">Total Applications</p>
@@ -71,10 +94,16 @@ const AppliedTutors = () => {
               <p className="text-xs sm:text-sm text-neutral">Selected</p>
               <p className="mt-1 text-2xl sm:text-3xl font-semibold text-base-content underline decoration-secondary/40">{totalSelected}</p>
             </div>
+
+            {/* Rejected */}
+            <div className="bg-base-100 border border-base-200 rounded-2xl p-4 sm:p-5 text-center shadow-[0_12px_30px_rgba(15,26,51,0.06)]">
+              <p className="text-xs sm:text-sm text-neutral">Rejected</p>
+              <p className="mt-1 text-2xl sm:text-3xl font-semibold text-base-content underline decoration-secondary/40">{totalRejected}</p>
+            </div>
           </div>
 
           {/* Tuition Card */}
-          {tuitions.map((tuition, i) => (
+          {appliedTuitions.map((tuition, i) => (
             <div
               key={i}
               className="mt-12 bg-base-100 rounded-3xl border border-base-200 shadow-[0_18px_45px_rgba(15,26,51,0.08)] p-6 sm:p-7"
@@ -108,7 +137,7 @@ const AppliedTutors = () => {
               <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-5">
                 {/* Card */}
                 {applications
-                  .filter((application) => application.tuitionId === tuition._id)
+                  .filter((application) => application.tuitionId === tuition._id && application.applyStatus === 'pending')
                   .map((application, i) => (
                     <div key={i} className="rounded-2xl border border-base-200 bg-base-100 p-5 shadow-sm">
                       <div className="flex flex-col md:flex-row items-center text-center md:text-left gap-4">
@@ -116,17 +145,33 @@ const AppliedTutors = () => {
                         <div>
                           <h4 className="font-semibold text-base-content">{application.tutorName}</h4>
                           <p className="text-xs sm:text-sm text-neutral">Qualification: {application.qualification}</p>
-                          <p className="text-sm font-semibold text-secondary mt-1">Expected Salary: {application.expectedSalary} ৳</p>
+                          <p className="my-2">
+                            <span className="font-semibold">Tutor Expected Salary:</span>{' '}
+                            <span className="font-semibold text-secondary">
+                              {application.expectedSalary ? (
+                                application.expectedSalary
+                              ) : (
+                                <span className="text-xs">(same as student budget)</span>
+                              )}{' '}
+                              <span className={application.expectedSalary ? 'inline' : 'hidden'}>৳</span>
+                            </span>{' '}
+                            <span className={application.expectedSalary ? 'text-neutral text-xs sm:text-sm' : 'hidden'}>/ month</span>
+                          </p>
                         </div>
                       </div>
 
-                      <div className="mt-4 flex flex-wrap gap-2">
+                      <div className="mt-4 flex flex-wrap gap-2 justify-center md:justify-start">
                         <button className="btn btn-primary btn-sm rounded-full px-4">Select</button>
-                        <button className="btn btn-ghost btn-sm rounded-full border border-base-300 px-4">Reject</button>
+                        <button
+                          onClick={() => handleRejectBtn(application)}
+                          className="btn btn-ghost btn-sm rounded-full border border-base-300 px-4"
+                        >
+                          Reject
+                        </button>
                         <button className="btn btn-outline btn-sm rounded-full px-4">Tutor Details</button>
                       </div>
 
-                      <p className="mt-2 text-[11px] text-neutral">Selection becomes final after payment</p>
+                      <p className="mt-2 text-[11px] text-neutral text-center md:text-left">Selection becomes final after payment</p>
                     </div>
                   ))}
               </div>
